44255afb0dc31a72eb40f276213fa48af6a712b7
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-05-28 14:02:17 -0500

    Turned off verbose wget output.

--- install-tengwar-scripts.sh
+++ install-tengwar-scripts.sh
@@ -23,7 +23,7 @@ mkdir --parents $FONT_DIR
 cd $FONT_DIR
 
 mkdir --parents parmaite
-wget --continue 'http://at.mansbjorkman.net/Downloads/Parmaite2.zip'
+wget --no-verbose --continue 'http://at.mansbjorkman.net/Downloads/Parmaite2.zip'
 unzip -u Parmaite2.zip -d $FONT_DIR/parmaite
 cd parmaite
 mv --verbose Parmaite.TTF Parmaite.ttf
@@ -32,21 +32,21 @@ cd -
 
 
 mkdir --parents elfica
-wget --continue 'http://www.oocities.org/enrombell/files/Pack_en.zip'
+wget --no-verbose --continue 'http://www.oocities.org/enrombell/files/Pack_en.zip'
 unzip -u Pack_en.zip -d $FONT_DIR/elfica
 cd elfica
 mv --verbose Elfica200841se07.ttf Elfica32.ttf
 cd -
 
 mkdir --parents gothika
-wget --continue 'http://www.oocities.org/enrombell/files/Gothika_en.zip'
+wget --no-verbose --continue 'http://www.oocities.org/enrombell/files/Gothika_en.zip'
 unzip -u Gothika_en.zip -d $FONT_DIR/gothika
 cd gothika
 mv --verbose Gothika2008-se001.ttf TengwarGothika050.ttf
 cd -
 
 mkdir --parents formal
-wget --continue 'http://tengwarformal.limes.com.pl/fonts/TengwarFormal-12c-ttf-pc.zip'
+wget --no-verbose --continue 'http://tengwarformal.limes.com.pl/fonts/TengwarFormal-12c-ttf-pc.zip'
 unzip -u TengwarFormal-12c-ttf-pc.zip -d $FONT_DIR/formal
 cd formal/TengwarFormal-12c-ttf-pc/fonts
 mv --verbose TengwarFormal12b.ttf TengwarFormal12.ttf
@@ -54,12 +54,12 @@ mv --verbose TengwarFormalA12b.ttf TengwarFormalA12.ttf
 cd -
 
 mkdir --parents annatar
-wget --continue -O annatar.zip 'http://web.comhem.se/alatius/fonts/tngan120.zip'
+wget --no-verbose --continue -O annatar.zip 'http://web.comhem.se/alatius/fonts/tngan120.zip'
 unzip -u annatar.zip -d $FONT_DIR/annatar
 #TODO: check that these all really match
 
 mkdir --parents quenya
-wget --continue -O quenya.zip 'http://img.dafont.com/dl/?f=tengwar_quenya'
+wget --no-verbose --continue -O quenya.zip 'http://img.dafont.com/dl/?f=tengwar_quenya'
 unzip -u quenya.zip -d $FONT_DIR/quenya
 cd quenya
 mv --verbose QUENYA.TTF TengwarQuenya.ttf
@@ -69,7 +69,7 @@ mv --verbose QUENCAP2.TTF TengwarQuenyaCapitals2.ttf
 cd -
 
 mkdir --parents sindarin
-wget --continue -O sindarin.zip 'http://img.dafont.com/dl/?f=tengwar_sindarin'
+wget --no-verbose --continue -O sindarin.zip 'http://img.dafont.com/dl/?f=tengwar_sindarin'
 unzip -u sindarin.zip -d $FONT_DIR/sindarin
 cd sindarin
 mv --verbose SINDAR.TTF TengwarSindarin.ttf
@@ -79,7 +79,7 @@ mv --verbose SINDCAP2.TTF TengwarSindarinCapitals2.ttf
 cd -
 
 mkdir --parents noldor
-wget --continue -O noldor.zip 'http://img.dafont.com/dl/?f=tengwar_noldor'
+wget --no-verbose --continue -O noldor.zip 'http://img.dafont.com/dl/?f=tengwar_noldor'
 unzip -u noldor.zip -d $FONT_DIR/noldor
 cd noldor
 mv --verbose NOLDOR.TTF TengwarNoldor.ttf
@@ -89,7 +89,7 @@ mv --verbose NOLDCAP2.TTF TengwarNoldorCapitals2.ttf
 cd -
 
 mkdir --parents teleri
-wget --continue -O teleri.zip 'http://img.dafont.com/dl/?f=tengwar_teleri'
+wget --no-verbose --continue -O teleri.zip 'http://img.dafont.com/dl/?f=tengwar_teleri'
 unzip -u teleri.zip -d $FONT_DIR/teleri
 cd teleri
 mv --verbose 'Tengwar Telerin.ttf' TengwarTelerin.ttf
@@ -98,7 +98,7 @@ cd -
 mkdir --parents ~/texmf/fonts/type1/tengwarscript/
 cd ~/texmf/fonts/type1/tengwarscript/
 mkdir --parents unicodeparmaite
-wget --continue 'http://www.uv.es/~conrad/UnicodeTengwarParmaite.tar.gz'
+wget --no-verbose --continue 'http://www.uv.es/~conrad/UnicodeTengwarParmaite.tar.gz'
 tar --ungzip --extract --overwrite --file UnicodeTengwarParmaite.tar.gz --directory unicodeparmaite
 cd unicodeparmaite
 mv --verbose parmaite.pfb UnicodeParmaite.pfb
