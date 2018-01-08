29cc8a43a52e90612922d4c723aacb69094cebac
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-01-08 15:09:41 -0600

    Reduce extraneous output.

--- install-tengwar-scripts.sh
+++ install-tengwar-scripts.sh
@@ -37,7 +37,6 @@ cd "$FONT_DIR"
 # later.
 
 mkdir -p parmaite
-printf "Downloading 'parmaite' font...\n"
 curl --remote-name 'http://at.mansbjorkman.net/Downloads/Parmaite2.zip'
 unzip -u Parmaite2.zip -d "$FONT_DIR/parmaite"
 cd parmaite
@@ -46,7 +45,6 @@ mv -v Parmaite_alt.TTF Parmaite_alt.ttf
 cd "$FONT_DIR"
 
 mkdir -p elfica
-printf "Downloading 'elfica' font...\n"
 curl --remote-name 'http://www.oocities.org/enrombell/files/Pack_en.zip'
 unzip -u Pack_en.zip -d "$FONT_DIR/elfica"
 cd elfica
@@ -54,7 +52,6 @@ mv -v Elfica200841se07.ttf Elfica32.ttf
 cd "$FONT_DIR"
 
 mkdir -p gothika
-printf "Downloading 'gothika' font...\n"
 curl --remote-name 'http://www.oocities.org/enrombell/files/Gothika_en.zip'
 unzip -u Gothika_en.zip -d "$FONT_DIR/gothika"
 cd gothika
@@ -62,7 +59,6 @@ mv -v Gothika2008-se001.ttf TengwarGothika050.ttf
 cd "$FONT_DIR"
 
 mkdir -p formal
-printf "Downloading 'formal' font...\n"
 curl --remote-name 'http://tengwarformal.limes.com.pl/fonts/TengwarFormal-12c-ttf-pc.zip'
 unzip -u TengwarFormal-12c-ttf-pc.zip -d "$FONT_DIR/formal"
 cd "formal/TengwarFormal-12c-ttf-pc/fonts"
@@ -71,30 +67,25 @@ mv -v TengwarFormalA12b.ttf TengwarFormalA12.ttf
 cd "$FONT_DIR"
 
 mkdir -p "annatar"
-printf "Downloading 'annatar' font...\n"
 curl --location --output annatar.zip 'http://web.archive.org/web/20150908132446/http://web.comhem.se/alatius/fonts/tngan120.zip'
 unzip -u annatar.zip -d "$FONT_DIR/annatar"
 
 mkdir -p "quenya"
-printf "Downloading 'quenya' font...\n"
 curl --location --output quenya.zip 'http://web.archive.org/web/20060816050032/http://www.acondia.com/font_tengwar/TengwarQuenya_v19E.zip'
 unzip -u quenya.zip -d "$FONT_DIR/quenya"
 cd "$FONT_DIR"
 
 mkdir -p "sindarin"
-printf "Downloading 'sindarin' font...\n"
 curl --location --output sindarin.zip 'http://web.archive.org/web/20060816050032/http://www.acondia.com/font_tengwar/TengwarSindarin_v19E.zip'
 unzip -u sindarin.zip -d "$FONT_DIR/sindarin"
 cd "$FONT_DIR"
 
 mkdir -p "noldor"
-printf "Downloading 'noldor' font...\n"
 curl --location --output noldor.zip 'http://web.archive.org/web/20060816050032/http://www.acondia.com/font_tengwar/TengwarNoldor_v19E.zip'
 unzip -u noldor.zip -d "$FONT_DIR/noldor"
 cd "$FONT_DIR"
 
 mkdir -p "teleri"
-printf "Downloading 'teleri' font...\n"
 curl --location --output teleri.zip 'http://img.dafont.com/dl/?f=tengwar_teleri'
 unzip -u teleri.zip -d "$FONT_DIR/teleri"
 cd teleri
@@ -104,16 +95,11 @@ cd "$FONT_DIR"
 mkdir -p ~/texmf/fonts/type1/tengwarscript/
 cd ~/texmf/fonts/type1/tengwarscript/
 mkdir -p "unicodeparmaite"
-printf "Downloading 'unicodeparmaite' font...\n"
 curl --remote-name 'http://www.uv.es/~conrad/UnicodeTengwarParmaite.tar.gz'
 tar -xf UnicodeTengwarParmaite.tar.gz -C unicodeparmaite
 cd "unicodeparmaite"
 mv -v parmaite.pfb UnicodeParmaite.pfb
 cd ~/texmf/fonts/type1/tengwarscript/
-
-GREEN='\033[0;32m'
-COLOROFF='\033[0m'
-printf "\n          %s*** Finished successfully. ***%s\n" "$GREEN" "$COLOROFF"
 
 # DONE: update names to match /usr/share/texlive/texmf-dist/fonts/map/dvips/tengwarscript/tengwarscript.map
 # DONE: install unicodeparmaite as well
